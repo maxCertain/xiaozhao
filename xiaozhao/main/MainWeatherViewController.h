a9396479baa8a27b6ce06440ceeece3087d93305
maxCertain <liicon@ABC.local>
2017-04-01 15:00:32 +0800

    tijao

---
+++ xiaozhao/main/MainWeatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainWeatherViewController.h
+//  xiaozhao
+//
+//  Created by liicon on 2017/4/1.
+//  Copyright © 2017年 max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainWeatherViewController : UIViewController
+
+@end
---
+++ xiaozhao/main/MainWeatherViewController.m
@@ -0,0 +1,39 @@
+//
+//  MainWeatherViewController.m
+//  xiaozhao
+//
+//  Created by liicon on 2017/4/1.
+//  Copyright © 2017年 max. All rights reserved.
+//
+
+#import "MainWeatherViewController.h"
+
+@interface MainWeatherViewController ()
+
+@end
+
+@implementation MainWeatherViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"大主人";
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
